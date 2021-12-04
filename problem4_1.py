37a9ba18820e032680ba38bcb2d2f8b32c0e2b6e
Yuri <brokendice7@gmail.com>
2021-12-04 14:50:47 +0100

    puzzle solutions day 4

---
+++ problem4_1.py
@@ -0,0 +1,64 @@
+def main():
+    f = open("day4.txt","r")
+    lines = f.readlines()
+    f.close()
+    tables = list()
+    grid = list()
+    numbers = [int(i) for i in lines[0].replace('\n','').split(',')]
+
+    for l in lines[2:]:
+        line = l.replace('\n','')
+        if line == "":
+            tables.append(Table(grid))
+            grid = list()
+            continue
+        grid.append([ int(i) for i in line.strip().replace('  ',' ').split(' ')] )
+    if len(grid) != 0:
+        tables.append(Table(grid))
+
+    i = 0
+    while i < len(numbers):
+        for table in tables:
+            table.check(numbers[i])
+            if table.is_winning():
+                for r in table.grid:
+                    print(r)
+                print(numbers[i] * table.result())
+                i = len(numbers)
+                break
+        i += 1
+
+class Table:
+    grid = list()
+
+    def __init__(self,grid):
+        self.grid = grid
+
+    def result(self):
+        return sum([sum(row) for row in self.grid])
+
+    def check(self,number):
+        for i in range(5):
+            row = self.grid[i]
+            if number in row:
+                #print("check",number)
+                row[row.index(number)] = 0
+                return True
+        return False
+
+    def is_winning(self):
+        for row in self.grid:
+            if sum(row) == 0:
+                return True
+        for i in range(5):
+            check = True
+            for j in range(5):
+                if self.grid[j][i] != 0:
+                    check = False
+                    break
+            if check: 
+                return True
+        return False
+
+if __name__ == "__main__":
+    main()
